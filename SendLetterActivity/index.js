3733c6b55d1dd623ef6ef30d143aa4382204e44e
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2020-06-02 10:58:08 +0200

    If there's more than one recipient in the letter, a separate forsendelse will be sent to each recipient.

--- SendLetterActivity/index.js
+++ SendLetterActivity/index.js
@@ -3,14 +3,26 @@ const svarut = require('@vtfk/svarut')(config.SVARUT)
 
 module.exports = async function (context) {
   const { letter } = context.bindings
-  context.log('Running activity!', letter)
+  context.log("Starting activity!")
 
   try {
-    const response = await svarut.sendForsendelse(letter)
-    return response
-  } catch (error) {
+    if (Array.isArray(letter.mottaker)) {
+      return letter.mottaker.map(async mottaker => {
+        try {
+          return await svarut.sendForsendelse({ ...letter, mottaker })
+        }
+        catch (error) {
+          context.error(JSON.stringify(error, null, 2))
+          return error
+        }
+      })
+    }
+
+    return await svarut.sendForsendelse(letter)
+  }
+  catch (error) {
     context.log(JSON.stringify(error, null, 2))
-    delete error.config.auth
+    if (error.config && error.config.auth) delete error.config.auth
     return error
   }
 }
