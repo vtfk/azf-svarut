5a67f6924f6448504ddc021d137cfbd2c7481b46
Mats Andreassen <matsand@live.no>
2020-06-02 13:07:47 +0200

    standard

--- SendLetterActivity/index.js
+++ SendLetterActivity/index.js
@@ -3,15 +3,14 @@ const svarut = require('@vtfk/svarut')(config.SVARUT)
 
 module.exports = async function (context) {
   const { letter } = context.bindings
-  context.log("Starting activity!")
+  context.log('Starting activity!')
 
   try {
     if (Array.isArray(letter.mottaker)) {
       return letter.mottaker.map(async mottaker => {
         try {
           return await svarut.sendForsendelse({ ...letter, mottaker })
-        }
-        catch (error) {
+        } catch (error) {
           context.error(JSON.stringify(error, null, 2))
           return error
         }
@@ -19,8 +18,7 @@ module.exports = async function (context) {
     }
 
     return await svarut.sendForsendelse(letter)
-  }
-  catch (error) {
+  } catch (error) {
     context.log(JSON.stringify(error, null, 2))
     if (error.config && error.config.auth) delete error.config.auth
     return error
