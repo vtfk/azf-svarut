0aec35846391f827c8b15f74feec06f68f0b882b
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2020-06-04 16:00:40 +0200

    Activity now waits on all forsendelser and just passes the result up th stream

--- SendLetterActivity/index.js
+++ SendLetterActivity/index.js
@@ -7,22 +7,18 @@ module.exports = async function (context) {
 
   try {
     if (Array.isArray(letter.mottaker)) {
-      return letter.mottaker.map(async mottaker => {
+      return await Promise.all(letter.mottaker.map(async mottaker => {
         try {
           return await svarut.sendForsendelse({ ...letter, mottaker })
         }
         catch (error) {
-          context.error(JSON.stringify(error, null, 2))
-          return error
+          throw error
         }
-      })
+      }))
     }
-
-    return await svarut.sendForsendelse(letter)
   }
   catch (error) {
-    context.log(JSON.stringify(error, null, 2))
     if (error.config && error.config.auth) delete error.config.auth
-    return error
+    throw error
   }
 }
