7aa27c164d914c8c1671cb1031d3e4e0fa5ef07d
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2020-06-04 16:02:37 +0200

    Retry is now handled by the caller. Validating the response to figure out customStatus

--- SendLetterOrchestrator/index.js
+++ SendLetterOrchestrator/index.js
@@ -1,13 +1,18 @@
 ﻿const df = require('durable-functions')
-const { RETRY } = require('../config')
+const checkResponse = require('../lib/checkResponse')
+//const { RETRY } = require('../config')
 
 module.exports = df.orchestrator(function * (context) {
   const letter = context.df.getInput()
   context.log('Orchestrator triggered!')
 
-  const retryOptions = new df.RetryOptions(RETRY.WAIT, RETRY.ATTEMPTS)
-  const response = yield context.df.callActivityWithRetry('SendLetterActivity', retryOptions, letter)
-  context.df.setCustomStatus(response.errorcode || response.stack ? 'Failed' : 'Sent')
+  //const retryOptions = new df.RetryOptions(RETRY.WAIT, RETRY.ATTEMPTS)
+  //const response = yield context.df.callActivityWithRetry('SendLetterActivity', retryOptions, letter)
+  const response = yield context.df.callActivity('SendLetterActivity', letter)
+  context.log("Got response:", response);
+  //context.df.setCustomStatus(response.errorcode !== undefined || response.stack ? 'Failed' : 'Sent')
+  const responseStatus = checkResponse(response, context)
+  context.df.setCustomStatus(responseStatus)
 
   return response
 })
